212bd84bfe0d570172bceda55b1d1cd2d5a0da9b
Lubeth <lubethjimenez@yahoo.com>
2026-03-08 17:34:14 -0400

    Add Specific GET Request Function

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
 
 console.log("Greetings Earthlings!");
 
+
+//Specific/GET Fetch Function//Only applies to specific fetch//
+//Typed 1st//
+async function starWars() {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1/");
+
+    if (!res.ok) {
+      return
+    }
+
+    const data = await res.json();
+    console.log(data)
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+starWars()
